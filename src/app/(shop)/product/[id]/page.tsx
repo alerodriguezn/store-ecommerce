d35a6cfba1f4a4bbc62e5bc011a3316bb0e3e8e6
alerodriguezn <navarrocmo@gmail.com>
2024-02-25 16:26:37 -0600

    style: add style for product page

--- src/app/(shop)/product/[id]/page.tsx
+++ src/app/(shop)/product/[id]/page.tsx
@@ -18,8 +18,8 @@ export default async function ProductPage({ params }: Props) {
   const { name, description, price, stockQuantity, imageUrl } = product;
 
   return (
-    <div className="flex justify-center">
-      <Image src={imageUrl} alt="asd" width={500} height={500} />
+    <div className="flex justify-center mt-4">
+      <Image src={imageUrl} alt="asd" width={500} height={500} className="rounded-md" />
 
       <div className="col-span-1 px-5">
         <span>Stock: {stockQuantity}</span>
@@ -30,8 +30,10 @@ export default async function ProductPage({ params }: Props) {
         </h1>
         <p className="text-lg mb-5">${price}</p>
 
-        <h3 className="font-bold text-sm">Description</h3>
-        <p className="font-light">{description}</p>
+        <button className="bg-blue-600 hover:bg-blue-800 text-white py-2 px-4 rounded transition-all">Agregar al carrito</button>
+
+        <h3 className="font-bold text-sm mt-2">Description</h3>
+        <p className="font-light max-w-[36ch]">{description}</p>
       </div>
     </div>
   );
