de7cf34e8cdd47280533ae6fa7c01d89539264fc
Minerva <minervacardenasm08@gmail.com>
2024-03-01 14:41:17 -0600

    feat:get all categories

--- src/actions/categories/get-all-categories.ts
+++ src/actions/categories/get-all-categories.ts
@@ -1,20 +1,27 @@
 'use server'
 
 
+import { RootObject } from "@/interfaces/products";
+
+
 export const getAllCategories = async () => {
 
 
     try {
+        const categories: RootObject = await fetch(`https://fake-store-api-409620.rj.r.appspot.com/api/categories/all?size=10`).then((res) => res.json());
+        
+        if (!categories) {
+            return {
+                ok: false,
+                message: "We cannot get the categories",
+            };
+        }
+    
+        return {
+            ok: true,
+            Allcategory: categories.content
 
-        //Obtener las categorias del api, hay que ponerle size=10 para que obtenga todas:
-        //https://fake-store-api-409620.rj.r.appspot.com/api/categories/all?size=10
-
-
-        //Despues de obtenerlas valida si se obtuvieron correctamente, si no, regresa un mensaje de error
-
-
-        //Si se obtuvieron correctamente, regresa un objeto con las categorias 
-
+        }
         
     } catch (error) {
         return {
