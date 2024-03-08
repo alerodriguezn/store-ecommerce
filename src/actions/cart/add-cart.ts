e94b4d3026d0ee2f41390452cca34b1a082c8eaf
Minerva <minervacardenasm08@gmail.com>
2024-03-08 15:35:23 -0600

    feat: add_cart

--- src/actions/cart/add-cart.ts
+++ src/actions/cart/add-cart.ts
@@ -1,20 +1,56 @@
 
 "use server"
 
-interface ProductInCart {
+import {NewCart} from '@/interfaces/cart'
+
+export interface ProductInCart {
     productId: number;
     quantity: number;
 }
 
 //Aqui tiene que recibir el userId: number, date: string, products: ProductInCart[]
-export const addCart = async () => {
+export const addCart = async (userId: number, date: string, products: ProductInCart[] ) => {
 
     //
     try {
+        const AddCartProduct: NewCart = await fetch('https://fakestoreapi.com/carts',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    userId: userId,
+                    date: date,
+                    products: products
+                }
+            )
+        })
+            .then(res=>res.json())
+    
 
-        //Hacer el fetch que le pase en la image
+    if (!AddCartProduct) {
+        return {
+            ok: false,
+            message: "We cannot add the cart",
+        };
+    }
+
+    return {
+        ok: true,
+        idcart: AddCartProduct.idcart,
+        userId,
+        date, 
+        products
+
+        
+
+    }
+
+        
         
     } catch (error) {
+        return {
+            ok: false,
+            message: "We can not add the cart",
+        }
         
     }
 
